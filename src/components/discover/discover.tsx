cf577c414286404b0a9c0713d4acb05e8aa2bd23
Alex Gorodov <a.gorodov.22@gmail.com>
2024-04-24 14:45:07 +0300

    history back from item

--- src/components/discover/discover.tsx
+++ src/components/discover/discover.tsx
@@ -1,12 +1,35 @@
 import { useSelector } from "react-redux";
 import { ShopItem } from "../shop-item/shop-item";
 import { RootState } from "../../store/RootState";
+import { useState } from "react";
 
 export function Discover(): JSX.Element {
-  const items = useSelector((state: RootState) => state.sell.items)
+  const originalItems = useSelector((state: RootState) => state.sell.items);
+  const [items, setItems] = useState([...originalItems]);
+
+  const sortByCategory = () => {
+    const sortedItems = [...originalItems].sort((a, b) => b.price - a.price);
+    setItems(sortedItems);
+  };
+
+  const sortByCollection = () => {
+    const sortedItems = [...originalItems].sort((a, b) => b.price - a.price);
+    setItems(sortedItems);
+  };
+
+  const sortByPrice = () => {
+    const sortedItems = [...originalItems].sort((a, b) => b.price - a.price);
+    setItems(sortedItems);
+  };
+
   return (
     <section className="section discover">
       <h1 className="title title--2 title--secondary">Discover NFTs</h1>
+      <div className="discover__buttons-wrapper">
+        <button className="button button--light" onClick={() => sortByCategory()}>Category</button>
+        <button className="button button--light" onClick={() => sortByCollection()}>Collection</button>
+        <button className="button button--light" onClick={() => sortByPrice()}>Price</button>
+      </div>
       <ul className="discover__list items__grid">
         {
           items.map((item) => {
--- src/pages/product/product-page.tsx
+++ src/pages/product/product-page.tsx
@@ -10,20 +10,20 @@ import { useSelector } from "react-redux";
 import { Layout } from "../../components/layout/layout";
 import { useIsMobileOnly } from "../../hooks/useIsMobile";
 import { users } from "../../mocks/users";
-import { AppRoute } from "../../const";
+import browserHistory from "../../browser-history";
 
 export function ProductPage(): JSX.Element {
   const { id } = useParams();
   const [isLoading, setIsLoading] = useState(true);
   const isMobile = useIsMobileOnly();
-
+  
   const [product, setProduct] = useState<Item | null>(null);
   const items = useSelector((state: RootState) => state.sell.items)
   useEffect(() => {
     const itemId = Number(id);
-    const foundUser = items.find((item) => item.id === itemId);
-    if (foundUser) {
-      setProduct(foundUser);
+    const foundProduct = items.find((item) => item.id === itemId);
+    if (foundProduct) {
+      setProduct(foundProduct);
     } else {
       setProduct(null);
     }
@@ -33,7 +33,6 @@ export function ProductPage(): JSX.Element {
   if (isLoading) {
     return <Spinner size={"40"} color={"#141416"}/>
   }
-
 
   if (!product) {
     return <NotFound/>
@@ -47,32 +46,32 @@ export function ProductPage(): JSX.Element {
       <main className="main">
         <section className="section section--centered product-page">
           <h1 className="visually-hidden">The page of {product.name}.</h1>
-          <Link className="button-wrapper product-page__breadcrumb-wrapper" to={AppRoute.Discover}>
-            <p className="button button--circle product-page__breadcrumb">
+          <div className="button-wrapper product-page__breadcrumb-wrapper">
+            <Link className="button button--circle product-page__breadcrumb"  to="#" onClick={() => browserHistory.back()}>
               <BackArrow/>
-            </p>
+            </Link>
             <p className="product-page__breadcrumb-title">Product Detail</p>
-          </Link>
+          </div>
           <div className="product-page__item">
             <img src={product.img} alt={product.name} width={isMobile ? 280 : 564} height={isMobile ? 280 : 564} />
             <div className="product-page__item-container">
               <h2 className="title title--2">{product.name}</h2>
               <p className="product-page__item-description">{product.description}</p>
               <div className="product-page__owners-wrapper">
-                <p className="product-page__owner">
+                <div className="product-page__owner">
                   <img className="product-page__owner-avatar" width={65} height={65} src={users[0].avatar} alt={`${users[0].firstname} ${users[0].surname.charAt(0)}.`} />
                   <p>
                     <span className="product-page__owner-description">Created by</span>
                     <span className="product-page__owner-name">{users[0].firstname}</span>
                   </p>
-                </p>
-                <p className="product-page__owner">
+                </div>
+                <div className="product-page__owner">
                   <img className="product-page__owner-avatar" width={65} height={65} src={users[1].avatar} alt={`${users[1].firstname} ${users[1].surname.charAt(0)}.`} />
                   <p>
                     <span className="product-page__owner-description">Owned by</span>
                     <span className="product-page__owner-name">{users[1].firstname}</span>
                   </p>
-                </p>
+                </div>
               </div>
             </div>
           </div>
